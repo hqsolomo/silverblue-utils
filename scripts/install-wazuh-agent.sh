94f70a8358545a91e7d34b5c33767b84ff0094b7
hqsolomo <36119229+hqsolomo@users.noreply.github.com>
2024-01-14 00:42:17 -0500

    Create install-wazuh-agent.sh

---
+++ scripts/install-wazuh-agent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --agent-name)
+            AGENT_NAME=$2
+            shift 2
+            ;;
+        --manager-host)
+            MANAGER_HOST=$2
+            shift 2
+            ;;
+        --agent-group)
+            AGENT_GROUP=$2
+            shift 2
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z $AGENT_NAME || -z $MANAGER_HOST || -z $AGENT_GROUP ]]; then
+    echo "Usage: $0 --agent-name NAME --manager-host HOST --agent-group GROUP"
+    exit 1
+fi
+
+toolbox create $AGENT_NAME
+toolbox run --container $AGENT_NAME "sudo WAZUH_MANAGER=$MANAGER_HOST WAZUH_AGENT_GROUP=$AGENT_GROUP WAZUH_AGENT_NAME=$AGENT_NAME yum install -y https://packages.wazuh.com/4.x/yum/wazuh-agent-4.5.2-1.x86_64.rpm"
+toolbox run --container $AGENT_NAME "sudo /var/ossec/bin/wazuh-control start"
