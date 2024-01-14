12659e72bf0a15b8816975ee60ddf161d3821f56
hqsolomo <36119229+hqsolomo@users.noreply.github.com>
2024-01-14 01:53:17 -0500

    Update install-wazuh-agent.sh

--- scripts/install-wazuh-agent.sh
+++ scripts/install-wazuh-agent.sh
@@ -14,6 +14,10 @@ while [[ $# -gt 0 ]]; do
             AGENT_GROUP=$2
             shift 2
             ;;
+        --agent-id)
+            AGENT_ID=$2
+            shift 2
+            ;;
         *)
             echo "Invalid argument: $1"
             exit 1
@@ -22,11 +26,17 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ -z $AGENT_NAME || -z $MANAGER_HOST || -z $AGENT_GROUP ]]; then
-    echo "Usage: $0 --agent-name NAME --manager-host HOST --agent-group GROUP"
+    echo "Usage: $0 --agent-name NAME --manager-host HOST --agent-group GROUP [--agent-id ID]"
     exit 1
 fi
 
+# Check if AGENT_ID is empty, if so, set it to the default value
+if [[ -z $AGENT_ID ]]; then
+    AGENT_ID="-agent"
+fi
+
 toolbox create $AGENT_NAME -y
-toolbox run --container $AGENT_NAME "sudo yum install -y https://packages.wazuh.com/4.x/yum/wazuh-agent-4.5.2-1.x86_64.rpm"
-toolbox run --container $AGENT_NAME "sudo WAZUH_MANAGER='$MANAGER_HOST' WAZUH_AGENT_GROUP='$AGENT_GROUP' WAZUH_AGENT_NAME='$AGENT_NAME'"
-toolbox run --container $AGENT_NAME "sudo /var/ossec/bin/wazuh-control start"
+# Rest of your script
+toolbox run --container $AGENT_NAME sudo WAZUH_MANAGER='$MANAGER_HOST' WAZUH_AGENT_GROUP='$AGENT_GROUP' WAZUH_AGENT_NAME='$AGENT_NAME$AGENT_ID' yum install -y https://packages.wazuh.com/4.x/yum/wazuh-agent-4.5.2-1.x86_64.rpm
+wait
+toolbox run --container $AGENT_NAME sudo /var/ossec/bin/wazuh-control start
